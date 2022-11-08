f8db126713d10e5888d8dd813c8bbbede9223c6e
Dave Edwards <humdings@gmail.com>
2022-11-08 13:09:06 -0800

    version bump to make release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='order_trees',
-    version="0.0.1",
+    version="0.0.2",
     description='Cached order book data structure.',
     url='https://github.com/humdings/order_trees',
     author='David Edwards',
@@ -17,6 +17,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3',
     ],
+    requires=['pandas'],
     platforms=['any'],
     packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
 )
